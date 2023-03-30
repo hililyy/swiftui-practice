9c523036dfb6e0d88997d50f9cb0ebccd12ecc6b
hililyy <joun406@gmail.com>
2023-03-30 22:48:57 +0900

    [ADD] Shape 주석 추가

--- SwiftUIBasic/ShapeBasic.swift
+++ SwiftUIBasic/ShapeBasic.swift
@@ -17,10 +17,9 @@ struct ShapeBasic: View {
 //                .foregroundColor(.pink)
 //                .stroke(Color.red, lineWidth: 20)
 //                .stroke(Color.purple, style: StrokeStyle(lineWidth: 30, lineCap: .round, dash: [30]))
-                .trim(from: 0.2, to: 1.0)
-                .stroke(Color.purple, lineWidth: 50)
-                .frame(width: 200, height: 100)
-                .padding(20)
+//                .trim(from: 0.2, to: 1.0)
+//                .frame(width: 200, height: 100)
+//                .padding(20)
             
             Text("타원형")
                 .font(.title)
@@ -56,3 +55,11 @@ struct ShapeBasic_Previews: PreviewProvider {
         ShapeBasic()
     }
 }
+
+
+/*
+ - VStack -> V는 vertical의 V임 (수평으로 나타내는 HStack도 있음)
+ - ~Stack으로 요소를 감싸서 나타내야 뷰에 나타남
+ - 새로운 VStack을 만들면 새로운 뷰가 생성됨 (휴대폰 하나 더 생성)
+ - trem -> 요소를 잘라먹음.. (그런 효과를 줄 수 있음)
+*/
